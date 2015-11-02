4fcc7e730117c6ed65b7c093df26ad57a6f8b5d0
RainC <support@rainclab.net>
2015-11-02 23:24:30 +0900

    fix google login

--- web-mainframe/src/js/controllers/signin.js
+++ web-mainframe/src/js/controllers/signin.js
@@ -6,22 +6,25 @@ app.controller('SigninFormController', ['$scope', '$http', '$state', function($s
     $scope.authError = null;
 
   //   $scope.login = function() {
-  //   	auth2.grantOfflineAccess({'redirect_uri': 'postmessage'}).then(signInCallback);
+  //    auth2.grantOfflineAccess({'redirect_uri': 'postmessage'}).then(signInCallback);
   //       //$state.go('app.list');
- 	// }
- 	$scope.processAuth = function(authResult) {
+    // }
+
+    $scope.processAuth = function(authResult) {
         // Do a check if authentication has been successful.
+        console.log(authResult);
         if(authResult['access_token']) {
             // Successful sign in.
             $scope.signedIn = true;
- 
+            $("#signInButton").hide();
+            $state.go('app.list');
             //     ...
             // Do some work [1].
             //     ...
         } else if(authResult['error']) {
             // Error while signing in.
             $scope.signedIn = false;
- 
+            $state.go('.access.signin');
             // Report error.
         }
     };
@@ -37,9 +40,12 @@ app.controller('SigninFormController', ['$scope', '$http', '$state', function($s
     $scope.renderSignInButton = function() {
         gapi.signin.render('signInButton',
             {
-               client_id: '863887952859-ol5bukmc38lkmfa82usslterdrrol4lc.apps.googleusercontent.com',
-		       scope : 'profile',
-
+                'callback': $scope.signInCallback, // Function handling the callback.
+                'clientid': '863887952859-ol5bukmc38lkmfa82usslterdrrol4lc.apps.googleusercontent.com', // CLIENT_ID from developer console which has been explained earlier.
+                'requestvisibleactions': 'http://schemas.google.com/AddActivity', // Visible actions, scope and cookie policy wont be described now,
+                                                                                  // as their explanation is available in Google+ API Documentation.
+                'scope': 'profile',
+                'cookiepolicy': 'single_host_origin'
             }
         );
     }
