0e5cd74ad38b983725c1d18bfb33b14ba3cc7e44
Jisu Kim <vbalien@live.jp>
2015-11-02 14:11:25 +0000

    added upload path set

---
+++ cloudmind/config/default_config.py
@@ -0,0 +1,11 @@
+ -*- coding: utf-8 -*-
+class Config(object):
+    # Database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+
+    # Path
+    UPLOAD_DIR = ''
+
+    # OAuth
+    OAUTH_GOOGLE_CLIENTID = ''
+    OAUTH_GOOGLE_SECRETKEY = ''
