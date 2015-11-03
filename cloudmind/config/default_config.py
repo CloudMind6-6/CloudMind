9cd3bcf10f6a291da25ec0aafb48aaa539322294
unknown <ham8952@gmail.com>
2015-11-04 00:23:44 +0900

    백엔드 default_config db위치 설정 값 변경

--- cloudmind/config/default_config.py
+++ cloudmind/config/default_config.py
@@ -1,7 +1,11 @@
 #  -*- coding: utf-8 -*-
+
+import os.path
+import tempfile
+
 class Config(object):
     # Database
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(tempfile.gettempdir(), 'test.db')
 
     # Path
     UPLOAD_DIR = ''
