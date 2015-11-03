5bba280ae46b7c94715fa7587a977642729f5214
HCH <ham8952@gmail.com>
2015-11-03 19:30:35 +0900

    마인드맵 라벨 추가 및 인원 추가

--- web-mainframe/src/js/app/mindmap/mindmap.js
+++ web-mainframe/src/js/app/mindmap/mindmap.js
@@ -2,8 +2,6 @@
  * mindmap controller
  */
 
-app.controller('MindmapCtrl', ['$scope', function($scope) {}]);
-
 label_palette = [
     {palette_number:0, name:"NONE", color:"#80FF80"},
     {palette_number:1, name:"WARNING", color:"#FFFF80"},
@@ -16,35 +14,35 @@ label_palette = [
 ];
 
 nodes = [
-    {idx:1, parent_idx:1, root_idx:1, name:"node1", desc:"asdfasdfasdf", assigned_user:[1001, 1002, 1003], labels:[0,1], due_date:"2015-11-19"},
-    {idx:2, parent_idx:1, root_idx:1, name:"node2", desc:"zxcv", assigned_user:[1001, 1002, 1003], labels:[1,2], due_date:"2015-11-19"},
-    {idx:3, parent_idx:2, root_idx:1, name:"node3", desc:"afawefeff", assigned_user:[1001, 1002, 1003], labels:[0,2], due_date:"2015-11-19"},
-    {idx:4, parent_idx:1, root_idx:1, name:"node4", desc:"AWEFAWVSZDF", assigned_user:[1001, 1002, 1003], labels:[5,6], due_date:"2015-11-19"},
-    {idx:5, parent_idx:4, root_idx:1, name:"node5", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:6, parent_idx:1, root_idx:1, name:"node6", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:7, parent_idx:1, root_idx:1, name:"node7", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:8, parent_idx:1, root_idx:1, name:"node8", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:1, parent_idx:1, root_idx:1, name:"node1", desc:"asdfasdfasdf", assigned_users:[1001, 1002, 1003], labels:[0,1], due_date:"2015-11-19"},
+    {idx:2, parent_idx:1, root_idx:1, name:"node2", desc:"zxcv", assigned_users:[1001, 1002, 1003], labels:[1,2], due_date:"2015-11-19"},
+    {idx:3, parent_idx:2, root_idx:1, name:"node3", desc:"afawefeff", assigned_users:[1001, 1002, 1003], labels:[0,2,1], due_date:"2015-11-19"},
+    {idx:4, parent_idx:1, root_idx:1, name:"node4", desc:"AWEFAWVSZDF", assigned_users:[1001, 1002, 1003], labels:[5,6,3], due_date:"2015-11-19"},
+    {idx:5, parent_idx:4, root_idx:1, name:"node5", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[4,7,5], due_date:"2015-11-19"},
+    {idx:6, parent_idx:1, root_idx:1, name:"node6", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[3,2,1], due_date:"2015-11-19"},
+    {idx:7, parent_idx:1, root_idx:1, name:"node7", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[1,4,6,3], due_date:"2015-11-19"},
+    {idx:8, parent_idx:1, root_idx:1, name:"node8", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,6], due_date:"2015-11-19"},
 
-    {idx:11, parent_idx:4, root_idx:1, name:"node11", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:12, parent_idx:4, root_idx:1, name:"node12", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:13, parent_idx:4, root_idx:1, name:"node13", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:11, parent_idx:4, root_idx:1, name:"node11", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:12, parent_idx:4, root_idx:1, name:"node12", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[6,7,1], due_date:"2015-11-19"},
+    {idx:13, parent_idx:4, root_idx:1, name:"node13", desc:"45345", assigned_users:[1001, 1002, 1003], labels:[2,3,0], due_date:"2015-11-19"},
 
-    {idx:14, parent_idx:7, root_idx:1, name:"node14", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:15, parent_idx:7, root_idx:1, name:"node15", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:16, parent_idx:7, root_idx:1, name:"node16", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:14, parent_idx:7, root_idx:1, name:"node14", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[5,1,3], due_date:"2015-11-19"},
+    {idx:15, parent_idx:7, root_idx:1, name:"node15", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:16, parent_idx:7, root_idx:1, name:"node16", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[2,3,1], due_date:"2015-11-19"},
 
-    {idx:17, parent_idx:16, root_idx:1, name:"node17", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:17, parent_idx:16, root_idx:1, name:"node17", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7,6], due_date:"2015-11-19"},
 
-    {idx:20, parent_idx:6, root_idx:1, name:"node20", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:21, parent_idx:6, root_idx:1, name:"node21", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:22, parent_idx:6, root_idx:1, name:"node22", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:20, parent_idx:6, root_idx:1, name:"node20", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:21, parent_idx:6, root_idx:1, name:"node21", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:22, parent_idx:6, root_idx:1, name:"node22", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
 
-    {idx:24, parent_idx:8, root_idx:1, name:"node24", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:24, parent_idx:8, root_idx:1, name:"node24", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
 
-    {idx:30, parent_idx:24, root_idx:1, name:"node30", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:31, parent_idx:24, root_idx:1, name:"node31", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:32, parent_idx:24, root_idx:1, name:"node32", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
-    {idx:33, parent_idx:24, root_idx:1, name:"node33", desc:"FAWEFAWEFAWG", assigned_user:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:30, parent_idx:24, root_idx:1, name:"node30", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:31, parent_idx:24, root_idx:1, name:"node31", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:32, parent_idx:24, root_idx:1, name:"node32", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
+    {idx:33, parent_idx:24, root_idx:1, name:"node33", desc:"FAWEFAWEFAWG", assigned_users:[1001, 1002, 1003], labels:[0,7], due_date:"2015-11-19"},
 ];
 
 
@@ -224,3 +222,11 @@ nodes.forEach(function(node)
     node.x = node_scene_graph.x;
     node.y = node_scene_graph.y;
 });
+
+
+
+
+app.controller('MindmapCtrl', ['$scope', function($scope) {
+    $scope.label_palette = label_palette;
+    $scope.nodes = nodes;
+}]);
