296920abfccfb94b7b5717cfba6ab2cf64597175
Jisu Kim <vbalien@live.jp>
2015-11-04 18:24:17 +0000

    fix issue #3

--- cloudmind/restful/v1/root.py
+++ cloudmind/restful/v1/root.py
@@ -4,6 +4,7 @@ from flask import session
 from flask_restful import abort
 from flask_restful import Resource
 
+
 class RootList(Resource):
     def get(self):
         if 'user_id' not in session:
@@ -11,7 +12,9 @@ class RootList(Resource):
         participants = db.session.query(Participant).filter(Participant.user_id == session['user_id']).all()
         node_list = []
         for item in participants:
-            node_list.append(item.own_node.serialize_root)
+            # if is root node
+            if item.own_node.parent_node is None:
+                node_list.append(item.own_node.serialize_root)
         return {
             'success':  True,
             "node_list": node_list
