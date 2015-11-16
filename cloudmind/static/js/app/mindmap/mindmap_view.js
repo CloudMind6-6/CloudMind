4515b6f417fadf9193d84a346da986c5cab578c4
Chiheon Ham <ham8952@gmail.com>
2015-11-16 23:41:44 +0900

    마인드맵 리프 썸네일 정렬

--- cloudmind/static/js/app/mindmap/mindmap_view.js
+++ cloudmind/static/js/app/mindmap/mindmap_view.js
@@ -421,7 +421,8 @@ SceneGraph.prototype =
                 .attr("idx", node_idx)
                 .style("left", this.view_center_x + node.x - node.width/2 + "px")
                 .style("top", this.view_center_y + node.y - node.height/2 + "px")
-                .style("background-size", "100px 100px")
+                .style("background-size", "cover")
+                .style("background-position", "center")
                 .style("background-image", model.file_type.search("image") == 0 ? "url(/api/v1/leaf/"+model.id+"?t=true)" : "");
 
             this.updateName(node);
